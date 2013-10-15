ae987eed5f8a288ab3044644a6822e6dcdc7f1d2
Hakuyume <Hakuyume@users.noreply.github.com>
2013-10-16 00:12:10 +0900

    configure yatex

--- init.el
+++ init.el
@@ -32,5 +32,9 @@
 
 (add-hook 'doc-view-mode-hook 'auto-revert-mode)
 
+(setq auto-mode-alist (cons '("\\.tex$" . yatex-mode) auto-mode-alist))
+(autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
+(setq tex-command "platex")
+
 (require 'tramp)
 
