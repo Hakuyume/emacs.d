a910ad7e6f3bddb87f6cfba1363ca85bfbf3d5e4
Hakuyume <Hakuyume@users.noreply.github.com>
2016-04-01 14:49:30 +0900

    use autopep8 for python

--- inits/30-python.el
+++ inits/30-python.el
@@ -1,6 +1,7 @@
+(add-hook 'python-mode-hook 'indent-guide-mode)
+(add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
 (add-hook 'python-mode-hook 'jedi:setup)
 (eval-after-load "jedi-core"
   '(progn
      (setq jedi:complete-on-dot t)
      (add-to-list 'company-backends 'company-jedi)))
-(add-hook 'python-mode-hook 'indent-guide-mode)
