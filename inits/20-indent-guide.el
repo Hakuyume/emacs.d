118bc2e0ef483d65c6b554078b703bd30391fa6e
Hakuyume <Hakuyume@users.noreply.github.com>
2016-04-01 13:22:17 +0900

    configure indent-guide

---
+++ inits/20-indent-guide.el
@@ -0,0 +1,2 @@
+(eval-after-load "indent-guide"
+  '(set-face-foreground 'indent-guide-face "green"))
--- inits/30-python.el
+++ inits/30-python.el
@@ -3,3 +3,4 @@
   '(progn
      (setq jedi:complete-on-dot t)
      (add-to-list 'company-backends 'company-jedi)))
+(add-hook 'python-mode-hook 'indent-guide-mode)
