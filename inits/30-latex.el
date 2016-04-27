de516ee68ea8873dbebd9ed0b78022465270807b
Hakuyume <Hakuyume@users.noreply.github.com>
2016-04-27 09:57:50 +0900

    add yasnippet config for latex mode

--- inits/30-latex.el
+++ inits/30-latex.el
@@ -1,16 +1,18 @@
 (add-hook 'latex-mode-hook 'indent-guide-mode)
-(add-hook 'latex-mode-hook 'reftex-mode)
+(add-hook 'latex-mode-hook
+          (lambda ()
+            (make-local-variable 'helm-mini-default-sources)
+            (yas-minor-mode)
+            (add-to-list 'helm-mini-default-sources helm-source-yasnippet)
+            (reftex-mode)
+            (add-to-list 'helm-mini-default-sources helm-source-reftex)))
 (eval-after-load "reftex"
   '(progn
      (setq reftex-refstyle "\\ref")
      (push '("reftex" :regexp t) popwin:special-display-config)
-     (defvar helm-source--reftex
-       '((name . "reftex")
-         (candidates . (("cite" . reftex-citation)
-                        ("ref" . reftex-reference)
+     (defvar helm-source-reftex
+       '((name . "RefTeX")
+         (candidates . (("ref" . reftex-reference)
+                        ("cite" . reftex-citation)
                         ("label" . reftex-label)))
          (action . command-execute)))))
-(add-hook 'reftex-mode-hook
-          (lambda ()
-            (make-local-variable 'helm-mini-default-sources)
-            (add-to-list 'helm-mini-default-sources helm-source--reftex)))
