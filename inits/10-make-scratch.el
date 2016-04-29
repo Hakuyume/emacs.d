cd79a1f751faf0397ea3c539230fac5b61f05b82
Hakuyume <Hakuyume@users.noreply.github.com>
2016-04-29 15:02:21 +0900

    refine make scratch function

--- inits/10-make-scratch.el
+++ inits/10-make-scratch.el
@@ -2,14 +2,11 @@
   (progn
     (set-buffer (get-buffer-create "*scratch*"))
     (funcall initial-major-mode)
-    (erase-buffer)
-    (when (and initial-scratch-message (not inhibit-startup-message))
-      (insert initial-scratch-message))))
+    (erase-buffer)))
 (add-hook 'kill-buffer-query-functions
           (lambda ()
             (if (string= "*scratch*" (buffer-name))
-                (progn (make-scratch) nil)
-              t)))
+                (make-scratch) t)))
 (add-hook 'after-save-hook
           (lambda ()
             (unless (member (get-buffer "*scratch*") (buffer-list))
