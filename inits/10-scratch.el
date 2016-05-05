5ef4e14fde3b2f08ea18e62bec879d3661fa085b
Hakuyume <Hakuyume@users.noreply.github.com>
2016-05-06 07:09:17 +0900

    add save-buffer-custom for saving scratch

--- inits/10-scratch.el
+++ inits/10-scratch.el
@@ -1,3 +1,16 @@
+(defun random-str (len)
+  (substring
+   (secure-hash 'sha512
+                (concat (current-time-string) (number-to-string (random t))))
+   0 len))
+(defun save-buffer-custom ()
+  (interactive)
+  (if (string= "*scratch*" (buffer-name))
+      (let ((default-directory (concat "~/scratches/" (random-str 16))))
+        (save-buffer))
+    (save-buffer)))
+(global-set-key "\C-x\C-s" 'save-buffer-custom)
+
 (defun make-scratch ()
   (progn
     (set-buffer (get-buffer-create "*scratch*"))
