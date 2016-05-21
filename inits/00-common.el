eba372f9ef288036f0a7f973c369a094c5a1ba0e
Hakuyume <Hakuyume@users.noreply.github.com>
2016-05-22 00:15:30 +0900

    add font config for X Window

--- inits/00-common.el
+++ inits/00-common.el
@@ -31,3 +31,7 @@
 (when (eq window-system 'w32)
   (set-face-attribute 'default nil :family "Consolas" :height 110)
   (set-fontset-font nil '(#x80 . #x3FFFFF) (font-spec :family "Yu Gothic")))
+
+(when (eq window-system 'x)
+  (set-face-attribute 'default nil :family "Inconsolata" :height 110)
+  (set-fontset-font nil '(#x80 . #x3FFFFF) (font-spec :family "IPA Gothic")))
