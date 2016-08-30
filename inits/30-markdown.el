5815856434b64266553d409b1c2da7ba62a46439
Hakuyume <Hakuyume@users.noreply.github.com>
2016-08-30 17:51:39 +0900

    enable mathml in markdown preview

--- inits/30-markdown.el
+++ inits/30-markdown.el
@@ -1,7 +1,7 @@
 (eval-after-load "markdown-mode"
   '(progn
      (setq markdown-asymmetric-header t)
-     (setq markdown-command "pandoc -s -f markdown -t html")
+     (setq markdown-command "pandoc -s --mathml -f markdown -t html")
      (define-key markdown-mode-map (kbd "TAB") 'markdown-demote)
      (define-key markdown-mode-map (kbd "S-<iso-lefttab>") 'markdown-promote)
      (define-key markdown-mode-map "\C-c\C-c" 'markdown-preview)))
