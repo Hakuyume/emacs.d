2c9da39fe38f5355bdf6b69b8d0ebc5aae753b78
Hakuyume <Hakuyume@users.noreply.github.com>
2016-08-30 15:20:09 +0900

    disable scratch saving

--- inits/10-scratch.el
+++ inits/10-scratch.el
@@ -1,23 +1,3 @@
-(defun random-str (len)
-  (substring
-   (secure-hash 'sha512
-                (concat (current-time-string) (number-to-string (random t))))
-   0 len))
-(defun prefer-extension (mode)
-  (cdr (assoc mode
-              '((emacs-lisp-mode . ".el")
-                (haskell-mode . ".hs")
-                (latex-mode . ".tex")
-                (markdown-mode . ".md")
-                (cperl-mode . ".pl")
-                (python-mode . ".py")))))
-(defun save-buffer-custom ()
-  (interactive)
-  (if (string= "*scratch*" (buffer-name))
-      (write-file (concat "~/scratches/" (random-str 16) (prefer-extension major-mode)))
-    (save-buffer)))
-(global-set-key "\C-x\C-s" 'save-buffer-custom)
-
 (defun make-scratch ()
   (progn
     (set-buffer (get-buffer-create "*scratch*"))
