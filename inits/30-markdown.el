87ae90a53e4dac48deffc57b9bb80ca7987bc94a
Hakuyume <Hakuyume@users.noreply.github.com>
2016-08-30 15:21:10 +0900

    configure markdown-mode (use pandoc for preview)

---
+++ inits/30-markdown.el
@@ -0,0 +1,9 @@
+(eval-after-load "markdown-mode"
+  '(progn
+     (setq markdown-asymmetric-header t)
+     (setq markdown-command "pandoc -s -f markdown -t html")
+     (define-key markdown-mode-map (kbd "TAB") 'markdown-demote)
+     (define-key markdown-mode-map (kbd "S-<iso-lefttab>") 'markdown-promote)
+     (define-key markdown-mode-map "\C-c\C-c" 'markdown-preview)))
+(when (package-installed-p 'indent-guide)
+  (add-hook 'markdown-mode-hook 'indent-guide-mode))
