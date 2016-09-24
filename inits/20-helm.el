138e77bcd3ba6cec25f7a69d6eee64cd086814d7
Hakuyume <Hakuyume@users.noreply.github.com>
2016-09-24 19:47:18 +0900

    remove some of helm mini sources

--- inits/20-helm.el
+++ inits/20-helm.el
@@ -8,9 +8,7 @@
      (custom-set-variables
       '(helm-mini-default-sources
         '(helm-source-buffers-list
-          helm-source-recentf
-          helm-source-files-in-current-dir
-          helm-source-locate)))))
+          helm-source-recentf)))))
 (eval-after-load "magit"
   '(progn
      (define-key magit-mode-map "\C-xa" 'helm-mini)))
