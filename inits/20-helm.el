d1793f694bb27d252e1213cb068d442204d2385d
Hakuyume <Hakuyume@users.noreply.github.com>
2016-09-24 15:29:52 +0900

    pop helm frame

--- inits/20-helm.el
+++ inits/20-helm.el
@@ -2,6 +2,7 @@
 (global-set-key "\M-y" 'helm-show-kill-ring)
 (eval-after-load "helm"
   '(progn
+     (setq helm-display-function 'pop-to-buffer)
      (define-key helm-map "\C-h" 'delete-backward-char)
      (define-key helm-map (kbd "TAB") 'helm-execute-persistent-action)
      (custom-set-variables
