86848ac345a9079a7f0df8abb10974ca041d9afc
Hakuyume <Hakuyume@users.noreply.github.com>
2017-05-09 09:11:23 +0900

    use cargo

--- inits/30-rust.el
+++ inits/30-rust.el
@@ -1,7 +1,4 @@
 (add-hook 'rust-mode-hook 'racer-mode)
-(eval-after-load "racer"
-  '(progn
-     (setq racer-rust-src-path "/usr/src/rust/src/")))
-(add-hook 'racer-mode-hook
-          (lambda ()
-            (add-hook 'before-save-hook 'rust-format-buffer nil t)))
+(custom-set-variables
+ '(rust-format-on-save t)
+ '(rust-rustfmt-bin "~/.cargo/bin/rustfmt"))
