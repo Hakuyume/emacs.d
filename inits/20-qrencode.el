e852a82aef36c3824390ac7617ffab5884beddcc
Hakuyume <Hakuyume@users.noreply.github.com>
2017-06-04 12:26:31 +0900

    use svg format in qrencode

--- inits/20-qrencode.el
+++ inits/20-qrencode.el
@@ -3,7 +3,7 @@
   (progn
     (if (get-buffer "*qrencode*")
         (kill-buffer "*qrencode*"))
-    (call-process-region start end "qrencode" nil "*qrencode*" nil "-tUTF8")
+    (call-process-region start end "qrencode" nil "*qrencode*" nil "-tsvg")
     (switch-to-buffer-other-window "*qrencode*")
     (read-only-mode)
-    (goto-char 0)))
+    (image-mode)))
