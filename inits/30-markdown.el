1d78b79f363aa265ba9861a914149a407162c7aa
Hakuyume <Hakuyume@users.noreply.github.com>
2017-07-31 10:54:18 +0900

    pandoc graphviz support

--- inits/30-markdown.el
+++ inits/30-markdown.el
@@ -1,7 +1,9 @@
 (eval-after-load "markdown-mode"
   '(progn
      (setq markdown-asymmetric-header t)
-     (setq markdown-command "pandoc -s --mathml -f markdown -t html")
+     (if (executable-find "pandoc-filter-graphviz")
+         (setq markdown-command "pandoc -s --mathjax -F pandoc-filter-graphviz -f markdown -t html")
+       (setq markdown-command "pandoc -s --mathjax -f markdown -t html"))
      (define-key markdown-mode-map (kbd "TAB") 'markdown-demote)
      (define-key markdown-mode-map (kbd "S-<iso-lefttab>") 'markdown-promote)
      (define-key markdown-mode-map "\C-c\C-c" 'markdown-preview)))
