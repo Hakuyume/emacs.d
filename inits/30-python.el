1fb0fb62febea2d646d5f75e87704562e5818ec6
Hakuyume <Hakuyume@users.noreply.github.com>
2017-08-21 16:57:06 +0900

    use python venv

--- inits/30-python.el
+++ inits/30-python.el
@@ -4,5 +4,6 @@
 (add-hook 'python-mode-hook 'jedi:setup)
 (eval-after-load "jedi-core"
   '(progn
+     (setq jedi:environment-virtualenv '("python" "-m" "venv"))
      (setq jedi:complete-on-dot t)
      (add-to-list 'company-backends 'company-jedi)))
