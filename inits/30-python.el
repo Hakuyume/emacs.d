758b449dc4d56a34374a4e87c7954d8d3806a8c0
Hakuyume <Hakuyume@users.noreply.github.com>
2019-06-02 01:39:17 +0900

    save hook

--- inits/30-python.el
+++ inits/30-python.el
@@ -1,6 +1,7 @@
 (use-package lsp
   :custom (lsp-pyls-configuration-sources '("flake8"))
-  :hook (python-mode . lsp))
+  :hook ((python-mode . lsp)
+         (python-mode . (lambda () (add-hook 'before-save-hook 'lsp-format-buffer nil t)))))
 
 (use-package indent-guide
   :hook (python-mode . indent-guide-mode))
