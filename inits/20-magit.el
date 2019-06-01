3beb4a1c563c56f7d7b08b340f950480be7f2c9f
Hakuyume <Hakuyume@users.noreply.github.com>
2019-06-02 03:09:26 +0900

    config after load

--- inits/20-magit.el
+++ inits/20-magit.el
@@ -1,7 +1,9 @@
 (use-package magit
   :bind (("C-x m" . magit-status)
          :map magit-mode-map
-         ("C-x a" . helm-mini)
-         ("G" . (lambda () (interactive)
-                  (let ((default-directory (magit-toplevel)))
-                    (helm-grep-do-git-grep t))))))
+         ("C-x a" . helm-mini))
+  :config (use-package helm
+            :bind (:map magit-mode-map
+                        ("G" . (lambda () (interactive)
+                                 (let ((default-directory (magit-toplevel)))
+                                   (helm-grep-do-git-grep t)))))))
