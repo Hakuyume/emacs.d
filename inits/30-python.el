96f72076041866e8f2a5d3d8dbe4f49e3576c210
Hakuyume <Hakuyume@users.noreply.github.com>
2019-06-01 21:29:51 +0900

    lsp-format-buffer

--- inits/30-python.el
+++ inits/30-python.el
@@ -1,12 +1,5 @@
-(use-package jedi-core
-  :config (add-to-list 'company-backends 'company-jedi)
-  :custom
-     (jedi:complete-on-dot t)
-     (jedi:environment-virtualenv '("python" "-m" "venv"))
-     :hook (python-mode . jedi:setup))
-
-(use-package py-autopep8
-  :hook ((python-mode . py-autopep8-enable-on-save)))
-
-(add-hook 'python-mode-hook 'flycheck-mode)
+(add-hook 'python-mode-hook
+          (lambda ()
+            (lsp)
+            (add-hook 'before-save-hook 'lsp-format-buffer nil t)))
 (add-hook 'python-mode-hook 'indent-guide-mode)
