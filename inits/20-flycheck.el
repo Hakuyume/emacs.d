87bb171ee2d438f390e05ffa2ee4d35c848c5a1f
Hakuyume <Hakuyume@users.noreply.github.com>
2019-06-02 03:07:22 +0900

    config after load

--- inits/20-flycheck.el
+++ inits/20-flycheck.el
@@ -1,3 +1,4 @@
-(use-package helm-flycheck
-  :bind (:map flycheck-mode-map
-              ("C-x e" . helm-flycheck)))
+(use-package flycheck
+  :config (use-package helm-flycheck
+            :bind (:map flycheck-mode-map
+                        ("C-x e" . helm-flycheck))))
