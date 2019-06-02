3b9adf5b593b1f74ca24d0dc14e3000a58149566
Hakuyume <Hakuyume@users.noreply.github.com>
2019-06-02 18:25:41 +0900

    fix for -nw mode

--- inits/20-company.el
+++ inits/20-company.el
@@ -1,13 +1,16 @@
 (use-package color)
 
 (use-package company
-  :bind ("M-<tab>" . company-complete)
-  :config (let ((bg (face-attribute 'default :background)))
-            (custom-set-faces
-             `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
-             '(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
-             '(company-tooltip-common ((t (:inherit font-lock-constant-face))))
-             '(company-tooltip-annotation ((t (:inherit font-lock-comment-face))))
-             `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
-             `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))))
+  :bind (("C-M-i" . company-complete)
+         :map emacs-lisp-mode-map
+         ("C-M-i" . company-complete))
+  :config (when window-system
+            (let ((bg (face-attribute 'default :background)))
+              (custom-set-faces
+               `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
+               '(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
+               '(company-tooltip-common ((t (:inherit font-lock-constant-face))))
+               '(company-tooltip-annotation ((t (:inherit font-lock-comment-face))))
+               `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
+               `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5))))))))
   :hook (after-init . global-company-mode))
