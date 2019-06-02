a908062a4fc0561c14efd15abf03d31935c482b1
Hakuyume <Hakuyume@users.noreply.github.com>
2019-06-02 23:17:41 +0900

    use use-package

--- inits/20-transient.el
+++ inits/20-transient.el
@@ -1,26 +1,29 @@
-(use-package transient)
+(use-package dired)
 
-(define-transient-command transient-dashboard ()
-  [[("f" "Flycheck" helm-flycheck)]
-   [("g" "Grep" transient-dashboard-grep)]
-   [("h" "Helm" helm-mini)]
-   [("H" "Helm (large)" transient-dashboard-helm-large)]
-   [("j" "Goto Line" goto-line)]
-   [("m" "Magit" magit-status)]])
+(use-package transient
+  :bind (("C-t" . transient-dashboard)
+         :map dired-mode-map
+         ("C-t" . transient-dashboard))
+  :demand
+  :config (progn
+            (define-transient-command transient-dashboard ()
+              [[("f" "Flycheck" helm-flycheck)]
+               [("g" "Grep" transient-dashboard-grep)]
+               [("h" "Helm" helm-mini)]
+               [("H" "Helm (large)" transient-dashboard-helm-large)]
+               [("j" "Goto Line" goto-line)]
+               [("m" "Magit" magit-status)]])
 
-(define-transient-command transient-dashboard-grep ()
-  [[("g" "Git Grep" transient-dashboard-grep-git-grep)]
-   [("p" "Grep" helm-projectile-grep)]])
+            (define-transient-command transient-dashboard-grep ()
+              [[("g" "Git Grep" transient-dashboard-grep-git-grep)]
+               [("p" "Grep" helm-projectile-grep)]])
 
-(defun transient-dashboard-helm-large ()
-  (interactive)
-  (helm '(helm-source-buffers-list
-          helm-source-recentf
-          helm-source-projectile-files-list)))
+            (defun transient-dashboard-helm-large ()
+              (interactive)
+              (helm '(helm-source-buffers-list
+                      helm-source-recentf
+                      helm-source-projectile-files-list)))
 
-(defun transient-dashboard-grep-git-grep ()
-  (interactive)
-  (let ((default-directory (magit-toplevel))) (helm-grep-do-git-grep t)))
-
-(global-set-key (kbd "C-t") 'transient-dashboard)
-(define-key dired-mode-map (kbd "C-t") 'transient-dashboard)
+            (defun transient-dashboard-grep-git-grep ()
+              (interactive)
+              (let ((default-directory (magit-toplevel))) (helm-grep-do-git-grep t)))))
