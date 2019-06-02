281ce066c2e290d0dfd98b5245627344bb089d9f
Hakuyume <Hakuyume@users.noreply.github.com>
2019-06-02 16:41:37 +0900

    remove reftex config

--- inits/20-shackle.el
+++ inits/20-shackle.el
@@ -2,6 +2,5 @@
   :custom (shackle-rules
            '(
              (compilation-mode :align below)
-             ("^\*helm" :regexp t :align below)
-             ("^\*reftex" :regexp t :align below)))
+             ("^\*helm" :regexp t :align below)))
   :init (shackle-mode))
