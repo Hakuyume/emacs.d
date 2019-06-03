d160b27dff585f9d5b0c1bba991a47c61e25a21f
Hakuyume <Hakuyume@users.noreply.github.com>
2019-06-03 11:06:45 +0900

    add lsp

--- inits/20-transient.el
+++ inits/20-transient.el
@@ -12,11 +12,16 @@
                [("h" "Helm" helm-mini)]
                [("H" "Helm (large)" transient-dashboard-helm-large)]
                [("j" "Goto Line" goto-line)]
+               [("l" "LSP" transient-dashboard-lsp)]
                [("m" "Magit" magit-status)]])
 
             (define-transient-command transient-dashboard-grep ()
               [[("g" "Git Grep" transient-dashboard-grep-git-grep)]
                [("p" "Grep" helm-projectile-grep)]])
+
+            (defun transient-dashboard-grep-git-grep ()
+              (interactive)
+              (let ((default-directory (magit-toplevel))) (helm-grep-do-git-grep t)))
 
             (defun transient-dashboard-helm-large ()
               (interactive)
@@ -24,6 +29,7 @@
                       helm-source-recentf
                       helm-source-projectile-files-list)))
 
-            (defun transient-dashboard-grep-git-grep ()
-              (interactive)
-              (let ((default-directory (magit-toplevel))) (helm-grep-do-git-grep t)))))
+            (define-transient-command transient-dashboard-lsp ()
+              [[("d" "Definition" lsp-find-definition)]
+               [("i" "Implementation" lsp-find-implementation)]
+               [("r" "Rename" lsp-rename)]])))
