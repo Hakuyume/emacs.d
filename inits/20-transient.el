daf07f7bbe5c76e8c56eaaac7d02a280a2a32c66
Hakuyume <Hakuyume@users.noreply.github.com>
2019-06-08 12:14:05 +0900

    indent

--- inits/20-transient.el
+++ inits/20-transient.el
@@ -3,35 +3,36 @@
 (use-package transient
   :bind ("C-z" . transient-dashboard)
   :demand
-  :config (progn
-            (define-transient-command transient-dashboard ()
-              [[("e" "Flycheck" helm-flycheck)]
-               [("g" "Grep" transient-dashboard-grep)]
-               [("h" "Helm" helm-mini)]
-               [("H" "Helm (full)" transient-dashboard-helm-full)]
-               [("j" "Goto Line" goto-line)]
-               [("l" "LSP" transient-dashboard-lsp)]
-               [("m" "Magit" magit-status)]
-               [("z" "Suspend" suspend-frame)]])
+  :config
+  (progn
+    (define-transient-command transient-dashboard ()
+      [[("e" "Flycheck" helm-flycheck)]
+       [("g" "Grep" transient-dashboard-grep)]
+       [("h" "Helm" helm-mini)]
+       [("H" "Helm (full)" transient-dashboard-helm-full)]
+       [("j" "Goto Line" goto-line)]
+       [("l" "LSP" transient-dashboard-lsp)]
+       [("m" "Magit" magit-status)]
+       [("z" "Suspend" suspend-frame)]])
 
-            (define-transient-command transient-dashboard-grep ()
-              [[("g" "Git Grep" transient-dashboard-grep-git-grep)]
-               [("p" "Grep" helm-projectile-grep)]])
+    (define-transient-command transient-dashboard-grep ()
+      [[("g" "Git Grep" transient-dashboard-grep-git-grep)]
+       [("p" "Grep" helm-projectile-grep)]])
 
-            (defun transient-dashboard-grep-git-grep ()
-              (interactive)
-              (let ((default-directory (magit-toplevel))) (helm-grep-do-git-grep t)))
+    (defun transient-dashboard-grep-git-grep ()
+      (interactive)
+      (let ((default-directory (magit-toplevel))) (helm-grep-do-git-grep t)))
 
-            (defun transient-dashboard-helm-full ()
-              (interactive)
-              (use-package helm-projectile)
-              (let ((helm-mini-default-sources
-                     '(helm-source-buffers-list
-                       helm-source-recentf
-                       helm-source-projectile-files-list)))
-                (helm-mini)))
+    (defun transient-dashboard-helm-full ()
+      (interactive)
+      (use-package helm-projectile)
+      (let ((helm-mini-default-sources
+             '(helm-source-buffers-list
+               helm-source-recentf
+               helm-source-projectile-files-list)))
+        (helm-mini)))
 
-            (define-transient-command transient-dashboard-lsp ()
-              [[("d" "Definition" lsp-find-definition)]
-               [("i" "Implementation" lsp-find-implementation)]
-               [("r" "Rename" lsp-rename)]])))
+    (define-transient-command transient-dashboard-lsp ()
+      [[("d" "Definition" lsp-find-definition)]
+       [("i" "Implementation" lsp-find-implementation)]
+       [("r" "Rename" lsp-rename)]])))
