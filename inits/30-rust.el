a9a6b11408ca4a03e04d596d1ea096740b97f622
Hakuyume <Hakuyume@users.noreply.github.com>
2019-07-06 11:09:29 +0900

    configure rls for tramp session

--- inits/30-rust.el
+++ inits/30-rust.el
@@ -1,4 +1,11 @@
 (use-package lsp
+  :config
+  (lsp-register-client
+   (make-lsp-client :new-connection (lsp-tramp-connection "$HOME/.cargo/bin/rls")
+                    :major-modes '(rust-mode)
+                    :remote? t
+                    :server-id 'rls-remote
+                    :notification-handlers (ht ("window/progress" 'lsp-clients--rust-window-progress))))
   :custom
   (lsp-rust-clippy-preference "on")
   (lsp-rust-rls-server-command '("rustup" "run" "nightly" "rls"))
