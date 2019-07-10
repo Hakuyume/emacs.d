350ab00979a9466e24290566b33d2774618c6542
Hakuyume <Hakuyume@users.noreply.github.com>
2019-07-11 08:42:33 +0900

    use if-let

--- inits/20-qrencode.el
+++ inits/20-qrencode.el
@@ -1,8 +1,8 @@
 (defun qrencode (start end)
   (interactive "r")
   (progn
-    (if (get-buffer "*qrencode*")
-        (kill-buffer "*qrencode*"))
+    (if-let ((buffer (get-buffer "*qrencode*")))
+        (kill-buffer buffer))
     (call-process-region start end "qrencode" nil "*qrencode*" nil "-tsvg")
     (switch-to-buffer-other-window "*qrencode*")
     (read-only-mode)
