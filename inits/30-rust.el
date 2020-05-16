9b145d4f782017e16bab13e9401cd9813bca7f66
Hakuyume <Hakuyume@users.noreply.github.com>
2020-05-16 16:45:02 +0900

    update rust analyzer

--- inits/30-rust.el
+++ inits/30-rust.el
@@ -2,7 +2,7 @@
 
 (use-package lsp-mode
   :custom
-  ;; cargo install --git https://github.com/rust-analyzer/rust-analyzer.git --tag 2020-04-27 rust-analyzer
+  ;; cargo install --git https://github.com/rust-analyzer/rust-analyzer.git --tag 2020-05-11 rust-analyzer
   (lsp-rust-server 'rust-analyzer)
   (lsp-rust-clippy-preference "on")
   :hook
