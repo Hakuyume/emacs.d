d51290fc5fc1eb4762ffe9a46ef31c012b378827
Hakuyume <Hakuyume@users.noreply.github.com>
2020-08-24 09:42:30 +0900

    remove comment

--- inits/30-rust.el
+++ inits/30-rust.el
@@ -2,7 +2,6 @@
 
 (use-package lsp-mode
   :custom
-  ;; cargo install --git https://github.com/rust-analyzer/rust-analyzer.git --tag 2020-05-11 rust-analyzer
   (lsp-rust-server 'rust-analyzer)
   (lsp-rust-clippy-preference "on")
   :hook
