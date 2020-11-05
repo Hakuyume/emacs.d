b1a4a7ddd7bac746e5676558057c61d569a898c9
Hakuyume <Hakuyume@users.noreply.github.com>
2020-11-06 08:10:11 +0900

    fix

--- inits/30-dockerfile.el
+++ inits/30-dockerfile.el
@@ -1,6 +1,6 @@
 (use-package dockerfile-mode
   :config
   (add-hook 'dockerfile-mode-hook
-            (lambda () (setq-local tab-width 4))))
+            (lambda () (setq-local tab-width 4)))
   :mode
   "/Containerfile")
