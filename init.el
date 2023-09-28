3bec16fe55769971675aa9d711f776120ad83be5
Toru Ogawa <Hakuyume@users.noreply.github.com>
2023-09-28 09:35:31 +0900

    drop make-scratch

--- init.el
+++ init.el
@@ -30,13 +30,6 @@
 
 (setq initial-major-mode 'fundamental-mode)
 (setq initial-scratch-message "")
-(defun make-scratch ()
-  (unless (get-buffer "*scratch*")
-    (set-buffer (get-buffer-create "*scratch*"))
-    (funcall initial-major-mode)
-    (erase-buffer)))
-(add-hook 'after-save-hook 'make-scratch)
-(add-hook 'kill-buffer-hook 'make-scratch)
 
 (use-package company
   :bind
